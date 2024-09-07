b6457d2bf68a4b43d85016a99fd104a89b4c28ac
Claus Guttesen <kometen@gmail.com>
2024-09-07 09:58:25 +0200

    If strict is true enable more options.

--- src/main.rs
+++ src/main.rs
@@ -52,14 +52,22 @@ fn post_pwd(password_options: Json<PasswordOptions>) -> Json<Vec<Pwd>> {
 
     let c = password_options.count.unwrap_or_else(|| 5);
     let pwd_length = password_options.length.unwrap_or_else(|| 20);
-    let option_numbers = password_options.numbers.unwrap_or_else(|| true);
-    let option_lowercase_letters = password_options.lowercase_letters.unwrap_or_else(|| true);
-    let option_uppercase_letters = password_options.uppercase_letters.unwrap_or_else(|| true);
-    let option_symbols = password_options.symbols.unwrap_or_else(|| false);
-    let option_spaces = password_options.spaces.unwrap_or_else(|| true);
+    let mut option_numbers = password_options.numbers.unwrap_or_else(|| true);
+    let mut option_lowercase_letters = password_options.lowercase_letters.unwrap_or_else(|| true);
+    let mut option_uppercase_letters = password_options.uppercase_letters.unwrap_or_else(|| true);
+    let mut option_symbols = password_options.symbols.unwrap_or_else(|| false);
+    let mut option_spaces = password_options.spaces.unwrap_or_else(|| true);
     let option_exclude_similar_characters = password_options.exclude_similar_characters.unwrap_or_else(|| false);
     let option_strict = password_options.strict.unwrap_or_else(|| false);
 
+    if option_strict {
+        option_numbers = true;
+        option_lowercase_letters = true;
+        option_uppercase_letters = true;
+        option_symbols = true;
+        option_spaces = true;
+    }
+    
     let pg = PasswordGenerator {
         length: pwd_length,
         numbers: option_numbers,
