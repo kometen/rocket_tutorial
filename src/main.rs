7d8531f1d44fb8885c88ec0b6fd0457f639a8fc6
Claus Guttesen <kometen@gmail.com>
2024-09-07 20:04:50 +0200

    Move password-generation to a function.

--- src/main.rs
+++ src/main.rs
@@ -25,11 +25,11 @@ struct PasswordOptions {
 }
 
 impl PasswordOptions {
-    fn new() -> PasswordOptions {
+    fn new(count: usize, length: usize) -> PasswordOptions {
         PasswordOptions {
-            count: Some(5),
+            count: Some(count),
             // Default password length
-            length: Some(20),
+            length: Some(length),
             numbers: Some(true),
             lowercase_letters: Some(true),
             uppercase_letters: Some(true),
@@ -48,8 +48,28 @@ fn index() -> &'static str {
 
 #[post("/", data = "<password_options>")]
 fn post_pwd(password_options: Json<PasswordOptions>) -> Json<Vec<Pwd>> {
-    let password_options = password_options.into_inner();
+    generate_passwords(&password_options)
+}
 
+
+#[get("/pwd")]
+fn pwd() -> Json<Vec<Pwd>> {
+    pwd_count(5)
+}
+
+#[get("/pwd/<count>")]
+fn pwd_count(count: usize) -> Json<Vec<Pwd>> {
+    let c = match count {
+        1..=31 => count,
+        _ => 31
+    };
+
+    let password_options = PasswordOptions::new(c, 20);
+
+    generate_passwords(&password_options)
+}
+
+fn generate_passwords(password_options: &PasswordOptions) -> Json<Vec<Pwd>> {
     let c = password_options.count.unwrap_or_else(|| 5);
     let pwd_length = password_options.length.unwrap_or_else(|| 20);
     let mut option_numbers = password_options.numbers.unwrap_or_else(|| true);
@@ -67,52 +87,6 @@ fn post_pwd(password_options: Json<PasswordOptions>) -> Json<Vec<Pwd>> {
         option_symbols = true;
         option_spaces = true;
     }
-    
-    let pg = PasswordGenerator {
-        length: pwd_length,
-        numbers: option_numbers,
-        lowercase_letters: option_lowercase_letters,
-        uppercase_letters: option_uppercase_letters,
-        symbols: option_symbols,
-        spaces: option_spaces,
-        exclude_similar_characters: option_exclude_similar_characters,
-        strict: option_strict,
-    };
-
-    let mut pwd: Vec<Pwd> = Vec::with_capacity(c);
-    pg.generate(c).unwrap().into_iter()
-        .map(|x| {
-            pwd.push(Pwd {
-                password: x.clone(),
-                score: scorer::score(&analyzer::analyze(&x)).ceil() as u8
-            });
-        }).count();
-    Json(pwd)
-}
-
-
-#[get("/pwd")]
-fn pwd() -> Json<Vec<Pwd>> {
-    pwd_count(5)
-}
-
-#[get("/pwd/<count>")]
-fn pwd_count(count: usize) -> Json<Vec<Pwd>> {
-    let c = match count {
-        1..=31 => count,
-        _ => 31
-    };
-
-    let password_options = PasswordOptions::new();
-
-    let pwd_length = password_options.length.unwrap_or_else(|| 20);
-    let option_numbers = password_options.numbers.unwrap_or_else(|| true);
-    let option_lowercase_letters = password_options.lowercase_letters.unwrap_or_else(|| true);
-    let option_uppercase_letters = password_options.uppercase_letters.unwrap_or_else(|| true);
-    let option_symbols = password_options.symbols.unwrap_or_else(|| false);
-    let option_spaces = password_options.spaces.unwrap_or_else(|| true);
-    let option_exclude_similar_characters = password_options.exclude_similar_characters.unwrap_or_else(|| false);
-    let option_strict = password_options.strict.unwrap_or_else(|| false);
 
     let pg = PasswordGenerator {
         length: pwd_length,
